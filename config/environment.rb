ef536d2ec1250bd5f3a7ca36a3c7de64ac1c3033
d3vkit <d3vkit@gmail.com>
2010-07-05 16:44:23 -0700

    added gems manifest file

--- config/environment.rb
+++ config/environment.rb
@@ -19,6 +19,10 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
+  config.gem "redgreen", :version => '1.2.2'
+  config.gem "rspec", :version => '1.3.0'
+  config.gem "rspec-rails", :version => '1.3.2'
+  config.gem "ZenTest", :version => '4.3.3'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
@@ -39,3 +43,4 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
 end
+
